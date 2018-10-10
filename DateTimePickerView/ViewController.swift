5512b6420f13082e51806c94962d03b70aba3642
affixusimac1 <viveking@outlook.com>
2018-10-10 12:47:35 +0530

    Datte

--- DateTimePickerView/ViewController.swift
+++ DateTimePickerView/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTimePicker: UIDatePicker!
+    @IBOutlet weak var lbDate: UILabel!
+    @IBOutlet weak var btoo: UIButton!
+    @IBOutlet weak var dateTime: UITextField!
+    
+    @IBOutlet weak var vwtp: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        dateTimePicker.isHidden = true
+        vwtp.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btOpen(_ sender: Any) {
+        dateTimePicker.isHidden = false
+        vwtp.isHidden = false
+    }
+    
+    @IBAction func picc(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.full
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        let strDate = dateFormatter.string(from: dateTimePicker.date)
+        lbDate.text = strDate
+        dateTime.text = strDate
+    }
+    
+    @IBOutlet weak var btDone: UIButton!
+    
+    @IBAction func btdd(_ sender: Any) {
+         dateTimePicker.isHidden = true
+        vwtp.isHidden = true
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        let strDate = dateFormatter.string(from: dateTimePicker.date)
+        lbDate.text = strDate
+        print("Date is \(strDate)")
+        self.view.endEditing(true)
+    }
+    
+   
 
+    
+    
 }
+
 
